d55303eac681fda83fdbc56f81ccf192a8761639
Gustavo Liedke <contact@gustavoliedke.com>
2017-09-04 16:49:57 +0100

    update menulink for footer
    we need to create a new compo for header nav menu

--- src/components/Menu/MenuLink/MenuLink.js
+++ src/components/Menu/MenuLink/MenuLink.js
@@ -17,17 +17,9 @@ class MenuLink extends Component {
     return (
 
       <li className="menu-item">
-
         {(item.link.url.indexOf('http') !== -1) ?
           <a href={item.link.url} target="_blank">{item.link.title}</a> :
-          <span className="menu-item__text">{item.link.title}</span>}
-
-        <ul className="main-nav__items menu--level-1">
-        {item.subtree.map((submenuItem, submenuItemKey) => {
-          return <SubMenuLink item={submenuItem} key={submenuItemKey}  />;
-        })}
-        </ul>
-
+          <a href={item.link.url}>{item.link.title}</a>}
       </li>
 
     )
