e6df4c04f8b5b195113f649c5c3723cff52a0a5b
Peter Vanhee <peter.vanhee@gmail.com>
2017-09-07 17:31:20 +0100

    Add SR footer example in sotrybook

--- src/stories/index.js
+++ src/stories/index.js
@@ -22,7 +22,14 @@ storiesOf('Footer', module)
   .add('Comic Relief', 
   	withInfo('doc string about my component')(() => {
   		const copy = text('Copy', 'copyright 2017');
-  		const source = 'http://pr-292-ip25kiy-3g6y4v7pqt6nk.eu.platform.sh/';
+  		const source = 'http://pr-292-ip25kiy-3g6y4v7pqt6nk.eu.platform.sh';
   		return (<Footer copy={copy} source={source}/>);
   	})
   )
+  .add('Sport Relief', 
+    withInfo('doc string about my component')(() => {
+      const copy = text('Copy', 'copyright 2018');
+      const source = 'http://pr-23-avxieja-wf5gj4v6kiu2o.eu.platform.sh';
+      return (<Footer copy={copy} source={source}/>);
+    })
+  );
