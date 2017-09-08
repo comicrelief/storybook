aaee155166864823cda236c6e561deaf5c636e9a
Peter Vanhee <peter.vanhee@gmail.com>
2017-09-08 16:48:53 +0100

    Fix ESLint

--- src/utils/drupal-api-connector.js
+++ src/utils/drupal-api-connector.js
@@ -1,36 +1,35 @@
-import {connect} from 'react-refetch'
-import urlJoin from 'url-join'
+import { connect } from 'react-refetch';
 
 const cache = new Map();
 
 export default connect.defaults({
-  buildRequest: function (mapping) {
+  buildRequest(mapping) {
     const options = {
       method: mapping.method,
       cache: 'force-cache',
       redirect: mapping.redirect,
-      body: mapping.body
+      body: mapping.body,
     };
     return new Request(mapping.url, options);
   },
 
-  fetch: function (input, init) {
+  fetch(input, init) {
     const req = new Request(input, init);
     const now = new Date().getTime();
     const inAMinute = 60000 + now;
     const cached = cache.get(req.url);
 
     if (cached && cached.time < inAMinute) {
-      return new Promise(resolve => resolve(cached.response.clone()))
+      return new Promise(resolve => resolve(cached.response.clone()));
     }
 
-    return fetch(req).then(response => {
+    return fetch(req).then((response) => {
       cache.set(req.url, {
         time: now,
-        response: response.clone()
+        response: response.clone(),
       });
 
-      return response
+      return response;
     });
-  }
+  },
 });
