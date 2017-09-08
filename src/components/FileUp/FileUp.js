09cab2e644c65148b623bce040b0527bb9a017a9
Gustavo Liedke <contact@gustavoliedke.com>
2017-09-08 17:02:40 +0100

    1 - accept pdf show error rejected files

--- src/components/FileUp/FileUp.js
+++ src/components/FileUp/FileUp.js
@@ -2,20 +2,23 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Dropzone from 'react-dropzone';
 import './styles.scss';
+import PdfImage from './Images/pdficon.png';
 
 class FileUp extends Component {
   constructor() {
     super();
     this.state = {
       files: [],
+      rejected: [],
     };
     this.handleImageReset = this.handleImageReset.bind(this);
     this.onDrop = this.onDrop.bind(this);
   }
 
-  onDrop(files) {
+  onDrop(files, rejected) {
+    console.log(rejected);
     const max = this.props.maxFiles;
-    const filesCombined = [... this.state.files, ...files];
+    const filesCombined = [...this.state.files, ...files];
 
     if (files.length > max) {
       this.setState({
@@ -37,6 +40,7 @@ class FileUp extends Component {
         });
       } else {
         this.setState({
+          rejected,
           files: filesCombined,
           error: '',
         });
@@ -67,7 +71,8 @@ class FileUp extends Component {
             <div className="fuga__img-uploaded">
               {this.state.files.map(file =>
                 (<span key={file.name} className="preview">
-                  <img src={file.preview} alt="" />
+                  {(file.type === 'application/pdf') ?
+                    <img src={PdfImage} alt="" /> : <img src={file.preview} alt="" />}
                   <p className="font--small font--centre"><a
                     className="link"
                     href="#"
@@ -78,15 +83,27 @@ class FileUp extends Component {
             </div>
             : null}
           {this.state.files.length < this.props.maxFiles ?
-            <Dropzone
-              className="dropzone"
-              multiple
-              accept="image/*"
-              onDrop={this.onDrop}
-            >
-              <p className="font--centre">Drop image here or <br />click to upload <br /></p>
-              <p className="cross">&#43;</p>
-            </Dropzone>
+            <div>
+              <Dropzone
+                className="dropzone"
+                multiple
+                accept="image/*, application/pdf"
+                onDrop={this.onDrop}
+              >
+                <p className="font--centre">Drop image here or <br />click to upload <br /></p>
+                <p className="cross">&#43;</p>
+              </Dropzone>
+            </div>
+            : null}
+          {this.state.rejected.length > 0 ?
+            <div>
+              <p>One or more files were rejected</p>
+              <ul>
+                {
+                  this.state.rejected.map(f => <li key={f.name}>{f.name} - {f.size} bytes</li>)
+                }
+              </ul>
+            </div>
             : null}
           {this.state.error ?
             <p className="font--small font--centre error">{this.state.error}</p>
