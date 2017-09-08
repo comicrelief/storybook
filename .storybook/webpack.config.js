4579ec526bd3258bfabb8af59af5372d728464ce
Gustavo Liedke <contact@gustavoliedke.com>
2017-09-08 17:01:29 +0100

    1 - add support img import

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -7,6 +7,18 @@ module.exports = {
         test: /\.scss$/,
         loaders: ["style-loader", "css-loader", "sass-loader"],
         include: path.resolve(__dirname, '../')
+      },
+      {
+        oneOf: [
+          {
+            test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+            loader: require.resolve('url-loader'),
+            options: {
+              limit: 10000,
+              name: 'static/media/[name].[hash:8].[ext]',
+            },
+          }
+        ]
       }
     ]
   }
