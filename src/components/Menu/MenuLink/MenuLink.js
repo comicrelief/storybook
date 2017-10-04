e51d9bfa2431a9365232a6c3d1485fc19c639958
Adam Clark <a.clark@comicrelief.com>
2017-10-04 10:29:14 +0100

    45 - Add method to add menu link to urls

--- src/components/Menu/MenuLink/MenuLink.js
+++ src/components/Menu/MenuLink/MenuLink.js
@@ -12,12 +12,15 @@ class MenuLink extends Component {
   render() {
     const item = this.props.item;
 
+
+    const url = item.link.url.indexOf('http') === -1 ? this.props.baseUrl + item.link.url : item.link.url;
+
     return (
 
       <li className="menu-item">
         {(item.link.url.indexOf('http') !== -1) ?
           <a href={item.link.url} target="_blank">{item.link.title}</a> :
-          <a href={item.link.url}>{item.link.title}</a>}
+          <a href={ url }>{item.link.title}</a>}
       </li>
 
     );
@@ -25,6 +28,7 @@ class MenuLink extends Component {
 }
 
 MenuLink.propTypes = {
+  baseUrl: PropTypes.string.isRequired,
   item: PropTypes.object.isRequired,
 };
 
