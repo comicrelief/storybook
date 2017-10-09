7ae10a420c0588f892d8b630195b880ca3127bdb
Jessie Wang <j.wang@comicrelief.com>
2017-10-09 12:27:58 +0100

    update svg

--- src/components/SocialLink/SocialLink.js
+++ src/components/SocialLink/SocialLink.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ReactSVG from 'react-svg';
-import Svg from '@comicrelief/pattern-lab/sass/base/components/svg/spritesheet.svg';
+import SvgSprite from '@comicrelief/pattern-lab/sass/base/components/svg/spritesheet.svg';
+/* svg with gradient background */
+import SvgInsta from '@comicrelief/pattern-lab/sass/base/components/svg/instagram.svg';
+
 /**
  * SocialLink class
  */
@@ -12,22 +15,18 @@ class SocialLink extends Component {
     const socialLinks = [{
         network: 'facebook',
         name: 'fb',
-        color: '#3d5b96',
         url: 'http://www.facebook.com/'+ campaign,
       }, {
         network: 'twitter',
         name: 'twitter',
-        color: '#1fb7ec',
         url: 'http://twitter.com/' + campaign,    
       }, {
         network: 'youtube',
         name: 'youtube',
-        color: '#cc3638',
         url: 'https://www.youtube.com/channel/UCdF5u0ggeSETozc8fsprjcw', 
       }, {
       network: 'instagram',
         name: 'instagram',
-        color: '#974b96',
         url: 'https://www.instagram.com/' + campaign, 
       },
     ]
@@ -35,17 +34,20 @@ class SocialLink extends Component {
     return (
       <div className="footer__social-links">
         <ReactSVG
-          path={Svg}
+          path={SvgSprite}
           callback={svg => console.log(svg)}
           className="visually-hidden"
         />
         <ul>
         { socialLinks.map(socialLink => (
-          <li>
-            <a href={socialLink.url} title={socialLink.network} target="_blank" className={`#icon__${socialLink.name}`}>
-              <svg className="icon">
-                <use xlinkHref={`#icon-${socialLink.network}`}></use>
-              </svg>
+          <li key={socialLink.network}>
+            <a href={socialLink.url} title={socialLink.network} target="_blank" className={`icon__${socialLink.name}`}>
+              { socialLink.network !== 'instagram' ? 
+                <svg className="icon">
+                  <use xlinkHref={`#icon-${socialLink.network}`}></use> 
+                </svg>:
+                <img src={SvgInsta} />
+              }
             </a>
           </li>
           )
