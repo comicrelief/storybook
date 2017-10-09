bd3550daaad6ce61296fda48913832d0248dd6e1
Jessie Wang <j.wang@comicrelief.com>
2017-10-09 11:08:45 +0100

    method 2 - svg sprite

--- src/components/SocialLink/SocialLink.js
+++ src/components/SocialLink/SocialLink.js
@@ -1,30 +1,32 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { SocialIcon } from 'react-social-icons';
-import './social-link.scss';
-
+import ReactSVG from 'react-svg';
+import Svg from '@comicrelief/pattern-lab/sass/base/components/svg/spritesheet.svg';
 /**
  * SocialLink class
  */
 class SocialLink extends Component {
 
   render() {
-
     const campaign = this.props.campaign
     const socialLinks = [{
         network: 'facebook',
+        name: 'fb',
         color: '#3d5b96',
         url: 'http://www.facebook.com/'+ campaign,
       }, {
         network: 'twitter',
+        name: 'twitter',
         color: '#1fb7ec',
         url: 'http://twitter.com/' + campaign,    
       }, {
         network: 'youtube',
+        name: 'youtube',
         color: '#cc3638',
         url: 'https://www.youtube.com/channel/UCdF5u0ggeSETozc8fsprjcw', 
       }, {
       network: 'instagram',
+        name: 'instagram',
         color: '#974b96',
         url: 'https://www.instagram.com/' + campaign, 
       },
@@ -32,10 +34,19 @@ class SocialLink extends Component {
 
     return (
       <div className="footer__social-links">
+        <ReactSVG
+          path={Svg}
+          callback={svg => console.log(svg)}
+          className="visually-hidden"
+        />
         <ul>
         { socialLinks.map(socialLink => (
-          <li key={socialLink.network}>
-            <SocialIcon url={socialLink.url} color={socialLink.color}/>
+          <li>
+            <a href={socialLink.url} title={socialLink.network} target="_blank" className={`#icon__${socialLink.name}`}>
+              <svg className="icon">
+                <use xlinkHref={`#icon-${socialLink.network}`}></use>
+              </svg>
+            </a>
           </li>
           )
         )}
