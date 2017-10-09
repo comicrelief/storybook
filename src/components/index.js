bc45d466ccbe8cf20925da41154f7467cc2bea84
Mohamed Labib <lebaz20@gmail.com>
2017-10-09 23:15:36 +0200

    fix: Add components index to load all components in external apps

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import Footer from './Footer/Footer';
+import SchoolsLookUp from './SchoolsLookUp/SchoolsLookUp';
+import FileUp from './FileUp/FileUp';
+
+export {
+	Footer,
+	SchoolsLookUp,
+	FileUp,
+}
