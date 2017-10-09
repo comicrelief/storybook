ffafb2ef0e78066589f3bb16b138da0c0e540e88
Mohamed Labib <lebaz20@gmail.com>
2017-10-09 23:33:38 +0200

    fix: config for babel and postcss

--- postcss.config.js
+++ postcss.config.js
@@ -2,9 +2,9 @@ module.exports = {
   plugins: {
     autoprefixer: {
       "browserslist": [
-				"> 1%",
-				"last 3 versions"
-			]
+        "> 1%",
+        "last 3 versions"
+      ]
     },
   },
 }
