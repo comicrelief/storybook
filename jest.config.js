bb68b3e28f3598649c953e9bd619c265c6925376
Mohamed Labib <lebaz20@gmail.com>
2017-10-31 17:16:39 +0200

    GP-891 Mock scss and svg files for jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   moduleNameMapper: {
-    '^.+\\.(css|less|scss)$': 'babel-jest',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less|scss)$': '<rootDir>/__mocks__/styleMock.js',
   },
 };
