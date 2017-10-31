53cd56d16d99462d73a13f57be40c7c41faa5f07
Mohamed Labib <lebaz20@gmail.com>
2017-10-31 17:33:40 +0200

    GP-891 handle requestAnimationFrame error

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
+  setupFiles: ['<rootDir>/__tests__/__mocks__/shim.js'],
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
-    '\\.(css|less|scss)$': '<rootDir>/__mocks__/styleMock.js',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__tests__/__mocks__/fileMock.js',
+    '\\.(css|less|scss)$': '<rootDir>/__tests__/__mocks__/styleMock.js',
   },
+  testPathIgnorePatterns: ['/__tests__/__mocks__/','/node_modules/'],
 };
