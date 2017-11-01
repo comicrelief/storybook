fcf0a2f305365dafdd2c79a9000147cca3ecc772
Mohamed Labib <lebaz20@gmail.com>
2017-11-01 15:44:28 +0200

    GP-891 move school lookup mock to shim to be effective everywhere

--- __tests__/stories/index.spec.js
+++ __tests__/stories/index.spec.js
@@ -1,14 +1,5 @@
 import initStoryshots from '@storybook/addon-storyshots';
 
-// React test renderer is not coupled to React DOM
-// here we have to mock findDOMNode as a result
-// it is used by react-onclickoutside used by react-bootstrap-typeahead
-jest.mock('react-dom', () => ({
-  findDOMNode: () => ({
-    getContext: jest.fn(),
-  }),
-}),
-);
 // test SchoolsLookUp layout at it's lifecycle different states
 initStoryshots({
   storyKindRegex: /^SchoolsLookUp$/,
