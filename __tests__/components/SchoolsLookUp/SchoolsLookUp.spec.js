27944659b0c88d876447c15faae050ebe2a18798
Mohamed Labib <lebaz20@gmail.com>
2017-11-01 16:50:33 +0200

    test: Add first school lookup interaction test
    Test 'or enter manually' click

--- __tests__/components/SchoolsLookUp/SchoolsLookUp.spec.js
+++ __tests__/components/SchoolsLookUp/SchoolsLookUp.spec.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import { shallow } from 'enzyme';
+
 import SchoolsLookUp from '../../../src/components/SchoolsLookUp/SchoolsLookUp';
-import renderer from 'react-test-renderer';
 
-describe('Addition', () => {
-  it('knows that 2 and 2 make 4', () => {
-    expect(2 + 2).toBe(4);
-  });
-});
-
-test('SchoolsLookUp displays error message when no result is found', () => {
-  const component = renderer.create(
+// 'or enter manually' button is clicked
+// search -> mock request when there are results and empty case
+// default option is there highlighted till other option is moved over
+// select school from results
+// edit button is clicked
+// insert manually with previously selected school
+// validation is triggered on blur
+test('selecting "enter manually" should display manual fields', () => {
+  const component = shallow(
     <SchoolsLookUp
       data=""
       min={2}
@@ -21,20 +23,23 @@ test('SchoolsLookUp displays error message when no result is found', () => {
       address3Value=""
       townValue=""
       postcodeValue=""
-    />
+    />,
   );
-  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-  console.log('sdsdsa');
-  // // manually trigger the callback
-  // tree.props.onMouseEnter();
-  // // re-rendering
-  // tree = component.toJSON();
-  // expect(tree).toMatchSnapshot();
+  // assert fields are not displayed before 'on enter manually' is clicked
+  expect(component.find('#establishmentName').exists()).toBe(false);
+  expect(component.find('#address1').exists()).toBe(false);
+  expect(component.find('#address2').exists()).toBe(false);
+  expect(component.find('#address3').exists()).toBe(false);
+  expect(component.find('#town').exists()).toBe(false);
+  expect(component.find('#postcode').exists()).toBe(false);
 
-  // // manually trigger the callback
-  // tree.props.onMouseLeave();
-  // // re-rendering
-  // tree = component.toJSON();
-  // expect(tree).toMatchSnapshot();
+  component.find('button[name="enterManually"]').simulate('click');
+
+  // assert fields are displayed after 'on enter manually' is clicked
+  expect(component.find('#establishmentName').exists()).toBe(true);
+  expect(component.find('#address1').exists()).toBe(true);
+  expect(component.find('#address2').exists()).toBe(true);
+  expect(component.find('#address3').exists()).toBe(true);
+  expect(component.find('#town').exists()).toBe(true);
+  expect(component.find('#postcode').exists()).toBe(true);
 });
