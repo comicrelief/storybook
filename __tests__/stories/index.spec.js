afcfd85102483adbadc40e6e37e08bc8ec02f9e9
Mohamed Labib <lebaz20@gmail.com>
2017-11-01 14:36:24 +0200

    GP-891 Fix snapshots test failure for school lookup - add school lookup ref snapshots

--- __tests__/stories/index.spec.js
+++ __tests__/stories/index.spec.js
@@ -1,3 +1,13 @@
 import initStoryshots from '@storybook/addon-storyshots';
 
+// React test renderer is not coupled to React DOM
+// here we have to mock findDOMNode as a result
+// it is used by react-onclickoutside used by react-bootstrap-typeahead
+jest.mock('react-dom', () => ({
+  findDOMNode: () => ({
+    getContext: jest.fn(),
+  }),
+}),
+);
+
 initStoryshots();
