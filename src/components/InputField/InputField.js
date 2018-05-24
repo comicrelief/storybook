8191dfdbc1d500c42d03961b05d71ae6e4c87afc
Denise Lashlley <deniselashlley@gmail.com>
2018-05-24 14:53:19 +0100

    fix arrow

--- src/components/InputField/InputField.js
+++ src/components/InputField/InputField.js
@@ -74,9 +74,7 @@ class InputField extends Component {
           pattern={this.props.pattern && this.props.pattern}
           aria-describedby={`field-label--${this.props.id} field-error--${this.props.id}`}
           onBlur={this.props.type !== 'checkbox' ? this.validateField : undefined}
-          onChange={e => this.props.type !== 'checkbox' ? this.props.inputHandler(e) : this.onChangeHandler(e)}
-
-
+          onChange={this.props.type !== 'checkbox' ? e => this.props.inputHandler(this.state.valid, e) : e => this.onChangeHandler(e)}
         />
         {this.props.type === 'checkbox' &&
         // span for checkbox styling
