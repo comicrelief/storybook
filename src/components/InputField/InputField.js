a39c271e03599a7c4bd4e7fe100925df57f1ffdf
Denise Lashlley <deniselashlley@gmail.com>
2018-06-07 15:38:53 +0100

    remove errorClassName

--- src/components/InputField/InputField.js
+++ src/components/InputField/InputField.js
@@ -65,9 +65,8 @@ class InputField extends Component {
     }
   }
   render() {
-    const errorClassName = this.state.valid === false || this.props.showErrorMessage === true ? 'form__field-error-wrapper' : '';
     return (
-      <div id={`field-wrapper--${this.props.id}`} className={`form__fieldset form__field-wrapper form__field-wrapper--${this.props.type} ${errorClassName} ${this.props.extraClass ? this.props.extraClass : ''} `}>
+      <div id={`field-wrapper--${this.props.id}`} className={`form__fieldset form__field-wrapper form__field-wrapper--${this.props.type} ${this.props.extraClass ? this.props.extraClass : ''} `}>
         <label id={`field-label--${this.props.id}`} htmlFor={`field-input--${this.props.id}`} className={`form__field-label${this.props.required ? ' required' : ''}`}>
           {this.props.label}
           {!this.props.required &&
