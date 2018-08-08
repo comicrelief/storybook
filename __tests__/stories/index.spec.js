f51f46bebde55f1a2fe09c619319ae0688100520
Mohamed Labib <lebaz20@gmail.com>
2018-08-08 05:49:07 +0200

    test: Fix jest pathes
    Use paths supported by jest, move any tests in src into tests dir.

--- __tests__/stories/index.spec.js
+++
@@ -1,6 +0,0 @@
-import initStoryshots from '@storybook/addon-storyshots';
-
-// test SchoolsLookUp layout at it's lifecycle different states
-initStoryshots({
-  storyKindRegex: /^SchoolsLookUp$/,
-});
