9cb664e32d48dc9b540c0396b8aa8723a8bdb194
Denise Lashlley <deniselashlley@gmail.com>
2018-10-15 14:39:11 +0100

    fix build error

--- src/components/PostcodeLookup/PostcodeLookup.js
+++ src/components/PostcodeLookup/PostcodeLookup.js
@@ -178,14 +178,15 @@ class PostcodeLookup extends Component {
   createAddressDropdownList() {
     const addresses = [{ label: 'Please select', value: null }];
 
-    if(this.state.addressLookupData !== undefined || this.state.addressLookupData !== null) {
-    this.state.addressLookupData.map(item =>
-      addresses.push({ label: typeof item.Line2 === 'undefined' ? item.Line1 : `${item.Line1}, ${item.Line2}`,
-        value: item }));
-    this.setState({
-      addressDropdownList: addresses,
-      isAddressSelectHidden: false,
-    });
+    if (this.state.addressLookupData !== undefined || this.state.addressLookupData !== null) {
+      this.state.addressLookupData.map(item =>
+        addresses.push({ label: typeof item.Line2 === 'undefined' ? item.Line1 : `${item.Line1}, ${item.Line2}`,
+          value: item }));
+      this.setState({
+        addressDropdownList: addresses,
+        isAddressSelectHidden: false,
+      });
+    }
   }
 
 
