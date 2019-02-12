a65845ee74ac7f5d7bbe6fb6a86d42a591139d0d
Heleen3258 <h.mol@comicrelief.com>
2019-02-12 13:58:52 +0100

    dealing with checkboxes without fields and some refactoring

--- src/components/MarketingConsent/MarketingConsentCheckbox.js
+++ src/components/MarketingConsent/MarketingConsentCheckbox.js
@@ -7,10 +7,10 @@ class MarketingConsentCheckbox extends Component {
   constructor(props) {
     super(props);
 
-    const items = props.itemData;
-    const checkbox = items.id;
-    // set the initial validation for the input fields
-    const fieldValidation = this.emptyInputFields(items);
+    const item = props.itemData;
+    const checkbox = item.id;
+    // set the initial validation for the input fields if they exist
+    const fieldValidation = item.field !== undefined ? this.emptyInputFields(item) : false;
 
     this.state = {
       checkboxId: checkbox,
@@ -92,21 +92,26 @@ class MarketingConsentCheckbox extends Component {
   }
 
   /**
-   * The handler enables the user to uncheck and check the checkbox.
-   * It reveals the previously hidden input field according to the selection.
-   * Resets the inputfield value.
+   * The handler enables the user to check and uncheck the checkbox.
+   * If the checkbox has fields, it reveals the previously hidden input field according to the selection and
+   * resets the input field value.
+   * Semi-toggles validation, value and showing the fields.
    */
-  handleCheckboxToggle(item, element, event) {
+  handleCheckboxToggle(item, option, event) {
     const value = event.target.value;
-    const fieldValidation = this.emptyInputFields(item);
+    // if item has fields empty them again.
+    const fieldValidation = this.state.checkboxValidation[item.id].fieldValidation === false ? false : this.emptyInputFields(item);
+    // if item has fields the options should tell you whether to show or hide the fields
+    const hideFields = fieldValidation === false ? true : option.hideFields;
     this.setState(prevState => ({
       checkboxValidation: {
         ...this.state.checkboxValidation,
         [item.id]: {
           ...this.state.checkboxValidation[item.id],
-          isFieldsHidden: prevState.checkboxValidation[item.id].value === value ? true : element.hideFields,
+          // check if value is the same to deal with unchecking the checkbox: toggles fields and validation.
+          isFieldsHidden: prevState.checkboxValidation[item.id].value === value ? true : hideFields,
+          valid: prevState.checkboxValidation[item.id].value === value ? true : hideFields,
           value: prevState.checkboxValidation[item.id].value !== value ? value : null,
-          valid: prevState.checkboxValidation[item.id].value === value ? true : element.hideFields,
           fieldValidation,
         },
       },
@@ -145,49 +150,49 @@ class MarketingConsentCheckbox extends Component {
         <p className="form__fieldset--label" aria-label={`Can we contact you by ${item.text}`}>{item.text}</p>
         <div id={`field-wrapper--${item.text}`} className="form__field--wrapper">
           {
-            item.options.map(element => (
-              <div key={element.value} className="form__field--wrapper form__checkbox form__checkbox--inline form__checkbox--inline-2-horizontal">
-                <label className="form__field-label required" htmlFor={`field-label--${element.label}`}>
-                  {element.label}
+            item.options.map(option => (
+              <div key={option.value} className="form__field--wrapper form__checkbox form__checkbox--inline form__checkbox--inline-2-horizontal">
+                <label className="form__field-label required" htmlFor={`field-label--${option.label}`}>
+                  {option.label}
                 </label>
                 <input
                   type="checkbox"
-                  id={`field-label--${element.label}`}
+                  id={`field-label--${option.label}`}
                   className="form__field form__field--checkbox"
-                  name={element.name}
-                  value={element.value}
-                  onChange={e => this.handleCheckboxToggle(item, element, e)}
-                  checked={this.state.checkboxValidation[checkbox].value === element.value}
+                  name={option.name}
+                  value={option.value}
+                  onChange={e => this.handleCheckboxToggle(item, option, e)}
+                  checked={this.state.checkboxValidation[checkbox].value === option.value}
                   ariarole="checkbox"
-                  aria-label={`field-label--${element.label}`}
-                  aria-checked={this.state.checkboxValidation[checkbox].value === element.value}
+                  aria-label={`field-label--${option.label}`}
+                  aria-checked={this.state.checkboxValidation[checkbox].value === option.value}
                 />
                 <span />
               </div>
             ))
           }
         </div>
-        { !this.state.checkboxValidation[checkbox].isFieldsHidden &&
+        { (!this.state.checkboxValidation[checkbox].isFieldsHidden && item.field) &&
         <div className={bgStyle}>
           {
-            item.field.map(element => (
-              <div key={element.id} className="form__field--wrapper form__field--sub-field-wrapper">
+            item.field.map(field => (
+              <div key={field.id} className="form__field--wrapper form__field--sub-field-wrapper">
                 <InputField
-                  type={element.type}
-                  id={element.name}
-                  name={element.name}
-                  required={element.required}
-                  placeholder={element.placeholder}
-                  label={element.label}
-                  pattern={element.pattern}
-                  helpText={element.helpText}
+                  type={field.type}
+                  id={field.name}
+                  name={field.name}
+                  required={field.required}
+                  placeholder={field.placeholder}
+                  label={field.label}
+                  pattern={field.pattern}
+                  helpText={field.helpText}
                   isValid={(valid, name) => {
                     this.setInputValidity(name, valid, checkbox);
                   }}
-                  emptyFieldErrorText={element.errorMessage}
+                  emptyFieldErrorText={field.errorMessage}
                   showErrorMessage={this.props.showErrorMessage}
-                  fieldValue={this.state.checkboxValidation[checkbox].fieldValidation[element.name]}
-                  value={() => this.fieldValue(checkbox, element.name)}
+                  fieldValue={this.state.checkboxValidation[checkbox].fieldValidation[field.name]}
+                  value={() => this.fieldValue(checkbox, field.name)}
                 />
               </div>
             ))
