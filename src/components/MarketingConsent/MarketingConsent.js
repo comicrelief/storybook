6fe849bc394f19f10037643c567eebdec03b12de
Heleen3258 <h.mol@comicrelief.com>
2019-02-18 10:35:50 +0100

    added fieldset div

--- src/components/MarketingConsent/MarketingConsent.js
+++ src/components/MarketingConsent/MarketingConsent.js
@@ -35,35 +35,37 @@ class MarketingConsent extends Component {
     const data = this.props.itemData;
     return (
       <div className={'form__row form__row--marketing-consent'} >
-        <p>Hear more about the project Comic Relief funds and other ways you can support our work including fundraising, campaigns and products.</p>
-        <p>How would you like to hear from us?</p>
-        { data.Questions.map(item =>
-          (<MarketingConsentCheckbox
-            key={item.id}
-            getValidation={(name, validation) => { this.updateState(name, validation); }}
-            itemData={item}
-            valueFromParent={this.props.valueFromParent && this.props.valueFromParent[item.id]}
-            showErrorMessages={this.props.showErrorMessages}
-          />)) }
-        <p>You can update your communication preferences at any time at&nbsp;
-          <a
-            href="https://www.comicrelief.com/update-your-preferences."
-            target="blank"
-            rel="noopener noreferrer"
-            className="link inline"
-          >
+        <div className="form__fieldset">
+          <p>Hear more about the project Comic Relief funds and other ways you can support our work including fundraising, campaigns and products.</p>
+          <p>How would you like to hear from us?</p>
+          { data.Questions.map(item =>
+            (<MarketingConsentCheckbox
+              key={item.id}
+              getValidation={(name, validation) => { this.updateState(name, validation); }}
+              itemData={item}
+              valueFromParent={this.props.valueFromParent && this.props.valueFromParent[item.id]}
+              showErrorMessages={this.props.showErrorMessages}
+            />)) }
+          <p>You can update your communication preferences at any time at&nbsp;
+            <a
+              href="https://www.comicrelief.com/update-your-preferences."
+              target="blank"
+              rel="noopener noreferrer"
+              className="link inline"
+            >
             comicrelief.com/update-your-preferences.
-            <span className="visuallyhidden">(opens in a new window)</span>
-          </a> Your details will be kept safe, check out our&nbsp;
-          <a
-            href="https://www.comicrelief.com/privacy-policy"
-            target="blank"
-            rel="noopener noreferrer"
-            className="link inline"
-          >
+              <span className="visuallyhidden">(opens in a new window)</span>
+            </a> Your details will be kept safe, check out our&nbsp;
+            <a
+              href="https://www.comicrelief.com/privacy-policy"
+              target="blank"
+              rel="noopener noreferrer"
+              className="link inline"
+            >
             privacy policy
-            <span className="visuallyhidden">(opens in a new window)</span>
-          </a> for more details.</p>
+              <span className="visuallyhidden">(opens in a new window)</span>
+            </a> for more details.</p>
+        </div>
       </div>
     );
   }
