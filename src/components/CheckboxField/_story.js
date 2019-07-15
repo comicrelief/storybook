348646bb9c26c3492c1303cc1f567bd02745bd70
Mohamed Labib <lebaz20@gmail.com>
2019-07-15 14:35:20 +0200

    #151 Fix data type in stories according to proptypes

--- src/components/CheckboxField/_story.js
+++ src/components/CheckboxField/_story.js
@@ -8,7 +8,7 @@ let type = 'checkbox';
 let id = 'checkedfield';
 let name = 'checkedfield';
 let label = 'Checkbox'
-let value = "1";
+let value = 1;
 let additionalText = '* By ticking I state I am a UK taxpayer making a personal donation and understand that if I pay less Income Tax and/or Capital Gains Tax than the amount of Gift Aid claimed on all my donations, it is my responsibility to pay any difference. <a href="https://www.comicrelief.com/frequently-asked-questions" class="link inline" target="_blank">Find out more</a>';
 
 storiesOf('CheckboxField', module)
