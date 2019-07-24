7fc9b018a2e2ceec4944c1fa3109bfcdc48ac3d0
Andy E Phipps <a.phipps@comicrelief.com>
2019-07-24 13:47:08 +0100

    Update Cypress snapshots

--- cypress/integration/components/Footer_spec.js
+++ cypress/integration/components/Footer_spec.js
@@ -1,16 +1,16 @@
 describe('Footer', () => {
   it('should load Comic Relief story', () => {
-    cy.loadStory('Footer', 'Comic Relief', 3000)
+    cy.loadStory('Footer', 'Comic Relief', 5000)
       .matchImageSnapshot();
   });
 
   it('should load Sport Relief story', () => {
-    cy.loadStory('Footer', 'Sport Relief', 3000)
+    cy.loadStory('Footer', 'Sport Relief', 5000)
       .matchImageSnapshot();
   });
 
   it('should load Red Nose Day story', () => {
-    cy.loadStory('Footer', 'Red Nose Day', 3000)
+    cy.loadStory('Footer', 'Red Nose Day', 5000)
       .matchImageSnapshot();
   });
 });
