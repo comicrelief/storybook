2893b1e0c1eb99471a0cfc3335f864d6a8449283
Andy E Phipps <a.phipps@comicrelief.com>
2019-07-29 09:53:03 +0100

    One final attempt

--- cypress/integration/components/Footer_spec.js
+++ cypress/integration/components/Footer_spec.js
@@ -1,16 +1,16 @@
 describe('Footer', () => {
   it('should load Comic Relief story', () => {
-    cy.loadStory('Footer', 'Comic Relief', 3000)
+    cy.loadStory('Footer', 'Comic Relief', 10000)
       .matchImageSnapshot();
   });
 
   it('should load Sport Relief story', () => {
-    cy.loadStory('Footer', 'Sport Relief', 3000)
+    cy.loadStory('Footer', 'Sport Relief', 10000)
       .matchImageSnapshot();
   });
 
   it('should load Red Nose Day story', () => {
-    cy.loadStory('Footer', 'Red Nose Day', 3000)
+    cy.loadStory('Footer', 'Red Nose Day', 10000)
       .matchImageSnapshot();
   });
 });
--- cypress/integration/components/SchoolsLookUp_spec.js
+++ cypress/integration/components/SchoolsLookUp_spec.js
@@ -1,26 +1,28 @@
+
+
 describe('Schools Lookup', () => {
   it('should load nothing is selected story', () => {
-    cy.loadStory('Schools Lookup', 'nothing is selected', 1000, 'SchoolsLookUp')
+    cy.loadStory('Schools Lookup', 'nothing is selected', 10000, 'SchoolsLookUp')
       .matchImageSnapshot();
   });
 
   it('should load manual fields errors story', () => {
-    cy.loadStory('Schools Lookup', 'manual fields errors', 1000, 'SchoolsLookUp')
+    cy.loadStory('Schools Lookup', 'manual fields errors', 10000, 'SchoolsLookUp')
       .matchImageSnapshot();
   });
 
   it('should load manually entered school story', () => {
-    cy.loadStory('Schools Lookup', 'manually entered school', 1000, 'SchoolsLookUp')
+    cy.loadStory('Schools Lookup', 'manually entered school', 10000, 'SchoolsLookUp')
       .matchImageSnapshot();
   });
 
   it('should load EDCO selected school story', () => {
-    cy.loadStory('Schools Lookup', 'EDCO selected school', 1000, 'SchoolsLookUp')
+    cy.loadStory('Schools Lookup', 'EDCO selected school', 10000, 'SchoolsLookUp')
       .matchImageSnapshot();
   });
 
   it('should load results', () => {
-    cy.loadStory('Schools Lookup', 'nothing is selected', 1000, 'SchoolsLookUp')
+    cy.loadStory('Schools Lookup', 'nothing is selected', 10000, 'SchoolsLookUp')
       .findByCustomId('school_lookup_input')
       .type('SE1')
       .wait(5000)
