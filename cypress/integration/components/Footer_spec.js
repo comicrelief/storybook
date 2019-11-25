025989d1ba4871f31c6e07257c239042c054bf3b
Adam Clark <a.clark@comicrelief.com>
2019-11-25 14:55:16 +0000

    Remove storybook test cr.com

--- cypress/integration/components/Footer_spec.js
+++ cypress/integration/components/Footer_spec.js
@@ -3,9 +3,4 @@ describe('Footer', () => {
     cy.loadStory('Footer', 'Sport Relief', 3000)
       .matchImageSnapshot();
   });
-
-  it('should load Red Nose Day story', () => {
-    cy.loadStory('Footer', 'Red Nose Day', 3000)
-      .matchImageSnapshot();
-  });
 });
