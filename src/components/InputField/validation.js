0c185a924c19cb8284ee06df00074b2bd45ba1fd
louiaR <louitiani@gmail.com>
2020-05-13 14:20:14 +0100

    Update email validation regex

--- src/components/InputField/validation.js
+++ src/components/InputField/validation.js
@@ -1,7 +1,8 @@
 const defaultValidationPatterns = {
   tel: /^[0-9 ]{11,}$/,
   number: /^[0-9]+$/,
-  email: /^([A-Za-z0-9_+-]+\.?)*[A-Za-z0-9_+-]+@[A-Za-z0-9]+([A-Za-z0-9_-]+\.?)*[A-Za-z0-9]+\.[A-Za-z]{2,}$/,
+  // eslint-disable-next-line no-useless-escape
+  email: /[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])/i,
   text: /^[\sA-Za-z0-9_.'&-]+$/,
 };
 
